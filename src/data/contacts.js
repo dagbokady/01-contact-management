3f4da900a474d8e27f0519daba8ad35015551f9e
Christ-Phanuel DAGBO <dagbokady@gmail.com>
2026-01-05 19:11:03 +0000

    update readme

---
+++ src/data/contacts.js
@@ -0,0 +1,84 @@
+const contacts = [
+    {
+        id: 1,
+        name: "Axelle Kouadio",
+        phone: "+225 07 89 45 23 11",
+        email: "axelle.kouadio@gmail.com",
+        category: "Famille",
+        photo: "https://i.pravatar.cc/150?img=47"
+    },
+    {
+        id: 2,
+        name: "Mohamed Traoré",
+        phone: "+225 05 66 78 90 34",
+        email: "mohamed.traore@outlook.com",
+        category: "Travail",
+        photo: "https://i.pravatar.cc/150?img=12"
+    },
+    {
+        id: 3,
+        name: "Nadia Koné",
+        phone: "+225 01 23 45 67 89",
+        email: "nadia.kone@yahoo.fr",
+        category: "Amis",
+        photo: "https://i.pravatar.cc/150?img=32"
+    },
+    {
+        id: 4,
+        name: "Jean-Marc Yao",
+        phone: "+225 07 11 22 33 44",
+        email: "jeanmarc.yao@gmail.com",
+        category: "Travail",
+        photo: "https://i.pravatar.cc/150?img=56"
+    },
+    {
+        id: 5,
+        name: "Fatou Diabaté",
+        phone: "+225 05 98 76 54 21",
+        email: "fatou.diabate@hotmail.com",
+        category: "Famille",
+        photo: "https://i.pravatar.cc/150?img=18"
+    },
+    {
+        id: 6,
+        name: "Eric N'Guessan",
+        phone: "+225 01 88 99 77 66",
+        email: "eric.nguessan@gmail.com",
+        category: "Amis",
+        photo: "https://i.pravatar.cc/150?img=25"
+    },
+    {
+        id: 7,
+        name: "Aminata Coulibaly",
+        phone: "+225 07 44 55 66 77",
+        email: "aminata.coulibaly@yahoo.com",
+        category: "Clients",
+        photo: "https://i.pravatar.cc/150?img=40"
+    },
+    {
+        id: 8,
+        name: "Koffi Assan",
+        phone: "+225 05 12 34 56 78",
+        email: "koffi.assan@company.ci",
+        category: "Travail",
+        photo: "https://i.pravatar.cc/150?img=9"
+    },
+    {
+        id: 9,
+        name: "Estelle Brou",
+        phone: "+225 01 67 89 01 23",
+        email: "estelle.brou@gmail.com",
+        category: "Clients",
+        photo: "https://i.pravatar.cc/150?img=61"
+    },
+    {
+        id: 10,
+        name: "Serge Aka",
+        phone: "+225 07 90 12 34 56",
+        email: "serge.aka@proton.me",
+        category: "Autres",
+        photo: "https://i.pravatar.cc/150?img=3"
+    }
+];
+
+export default contacts;
